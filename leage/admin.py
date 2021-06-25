95b9334f8690aab8413d721cb8b1a7eeda8f3dd0
Michał Górkiewicz <michal.gorkiewicz@student.wat.edu.pl>
2021-06-25 14:41:18 +0200

    1

--- leage/admin.py
+++ leage/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from leage.models import Postac, Rola, Pozycja
 
+from leage.models import *
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Tag)
+admin.site.register(Order)
+
 admin.site.register(Postac)
 admin.site.register(Rola)
 admin.site.register(Pozycja)
