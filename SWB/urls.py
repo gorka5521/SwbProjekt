82255ce0ce971b559476856ce134e18fdd231497
Michał Górkiewicz <michal.gorkiewicz@student.wat.edu.pl>
2021-06-25 15:07:21 +0200

    1

--- SWB/urls.py
+++ SWB/urls.py
@@ -1,23 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from leage import views
+
 from leage.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
 
-    path('register/', views.registerPage, name="register"),
-    path('login/', views.loginPage, name="login"),
-    path('logout/', views.logoutUser, name="logout"),
-
-    path('', views.home, name="home"),
-    path('products/', views.products, name='products'),
-    path('customer/<str:pk_test>/', views.customer, name="customer"),
-
-    path('create_order/<str:pk>/', views.createOrder, name="create_order"),
-    path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
-    path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
 
 ]
